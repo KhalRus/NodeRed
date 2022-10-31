4a3be0fc55a7bbdd351aec1e469fc529519f6f27
KhalRus <KhalRusel@ya.ru>
2022-10-31 11:59:38 +0500

    Добавлен вывод сообщений в журнал и запись в Coil
    Вместо регистра 16бит команды ТУ пишутся в ячейки Coil,
    каждая команда в свою ячейку.
    Все сообщения выводятся в журнал
    Добавлен вывод имени устройства (объекта) для функции журнала

--- owen16R_on_start.js
+++ owen16R_on_start.js
@@ -1,15 +1,28 @@
-let signalsAll = ['ch_1', 'ch_2', 'ch_3', 'ch_4', 'ch_5', 'ch_6', 'ch_7', 'ch_8',
+let signals = ['ch_1', 'ch_2', 'ch_3', 'ch_4', 'ch_5', 'ch_6', 'ch_7', 'ch_8',
   'ch_9', 'ch_10', 'ch_11', 'ch_12', 'ch_13', 'ch_14', 'ch_15', 'ch_16',].map(st => env.get(st));
-context.set('signalLength', env.get('TU').length - 1);  // длина пути в названии топика
+
+let TU = env.get('TU');
+context.set('signalLength', TU.length - 1);                        // длина пути в названии топика
+context.set('tag', TU.slice(0, -1));    // путь без +   Nasosn2/Owen_16R_/
+context.set('signals', signals);
 
 let mess = {
   topic: 'initModbusWrite',
   addres: env.get('modbus_addr'),
   devname: env.get('dev_name'),
-  reg: 50,
-  type: 'bitArr',
-  payload: signalsAll,
-  command: 16,
+  type: 'word',
+  payload: 0,
+  command: 15,
 };
 
-node.send([mess, null]);
+let logName = {    // Имя устройства в журнале (объект)
+  payload: {
+    str: 'logName',
+    id: env.get('dev_name'),
+    logName: env.get('log_name'),
+  },
+  topic: `${context.get('tag')}log`,
+  retain: true,
+}
+
+node.send([mess, logName]);
--- owen8R.js
+++ owen8R.js
@@ -1,20 +1,75 @@
-const dev_name = env.get('dev_name');
-let mess = [null, null]; // выходы функции, null - ничего не отправляется на выход
+const INFO = 0;  // типы сообщений в логе
+const ALERT = 1;
+const ERROR = 2;
 
-if (msg.topic == 'okWrite') {
-  node.status({ fill: 'green', shape: 'dot', text: `ok - ${msg.payload}` });
+let mess = [null, null]; // выходы функции, 0 - ТУ в модбас, 1 - сообщение в журнал
+const MS_TU = 0;
+const MS_LOG = 1;
 
-} else if (msg.topic == 'errorWrite') {
-  node.status({ fill: 'yellow', shape: 'ring', text: `err - ${msg.payload}` });
-  // вывод ошибки в журнал потом добавить
-
-} else {
-  let signal = msg.topic.slice(context.get('signalLength'));  // только название сигнала без пути
-
-  mess[0] = {
-    payload: msg.payload == 'true',
-    topic: 'writeVar',
-    signal: signal,
-  };
+let topic = msg.topic;
+if (topic.startsWith(context.get('tag'))) {
+  topic = msg.topic.slice(context.get('signalLength'));   // только название сигнала без пути
 }
+
+if (topic == 'okWrite') {
+  node.status({ fill: 'green', shape: 'dot', text: `ok - ${msg.payload}` });
+  mess[MS_LOG] = {
+    payload: {
+      str: `Команда записана успешно: ${msg.payload}`,
+      type: INFO,
+    },
+  };
+
+} else if (topic == 'errorWrite') {
+  node.status({ fill: 'yellow', shape: 'ring', text: `err - ${msg.payload}` });
+  mess[MS_LOG] = {
+    payload: {
+      str: `Ошибка записи команды: ${msg.payload}`,
+      type: ERROR,
+    },
+  };
+
+} else if (topic == 'errorUnknown') {
+  mess[MS_LOG] = {
+    payload: {
+      str: msg.payload,
+      type: ERROR,
+    },
+  };
+
+} else if (topic == 'log') {  // пришло сообщение в журнал, здесь его не обрабатываем
+
+} else {                                                      // остальные сигналы это команды ТУ
+  let ind = context.get('signals').indexOf(topic);           // индекс в массиве соответствует регистру COIL
+
+  if ((ind != -1) && (topic != 'null')) {
+    mess[MS_TU] = {
+      payload: [(msg.payload === true) || (msg.payload == 'true') || (msg.payload == 1) || (msg.payload == '1')],
+      topic: 'writeVar',
+      reg: ind,
+      signal: topic,
+    };
+
+    mess[MS_LOG] = {
+      payload: {
+        str: `Подана команда ТУ: ${topic} - ${msg.payload}`,
+        type: INFO,
+      },
+    };
+
+  } else {  // команда в массиве команд не найдена
+    mess[MS_LOG] = {
+      payload: {
+        str: `Неправильная команда ТУ: ${topic} - ${msg.payload}`,
+        type: ERROR,
+      },
+    };
+  }
+}
+
+if (mess[MS_LOG] !== null) {  // добавляем в MQTT метку времени и топик (одно и тоже для всех сообщений, добавляем здесь, чтобы не дублировать это везде)
+  mess[MS_LOG].topic = `${context.get('tag')}log`;
+  mess[MS_LOG].payload.time = Date.now();
+}
+
 return mess;
